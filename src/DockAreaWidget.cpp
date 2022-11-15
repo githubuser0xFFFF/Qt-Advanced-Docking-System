b9718cdd5ddfb1b533195e8deda2a3b712795a27
fb <f@b.com>
2022-11-15 21:23:35 +0100

    CDockAreaWidget::removeDockWidget: guard against nullpointer crash

--- src/DockAreaWidget.cpp
+++ src/DockAreaWidget.cpp
@@ -452,6 +452,8 @@ void CDockAreaWidget::insertDockWidget(int index, CDockWidget* DockWidget,
 void CDockAreaWidget::removeDockWidget(CDockWidget* DockWidget)
 {
     ADS_PRINT("CDockAreaWidget::removeDockWidget");
+    if(DockWidget == nullptr)
+        return;
     auto CurrentDockWidget = currentDockWidget();
   	auto NextOpenDockWidget = (DockWidget == CurrentDockWidget) ? nextOpenDockWidget(DockWidget) : nullptr;
 
